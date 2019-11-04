3cecbc0c654172cb1eb007d9ebb7984c5b540123
Caio Henrique Silva Franco <caiohenriquesilvafranco@MacBook-Pro-de-Caio-Franco.local>
2019-11-04 16:05:03 -0300

    Fisrt Commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Plant App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
