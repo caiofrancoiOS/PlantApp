e36393be57860374148cfb679aae675618b4c337
caiohenriquesilvafranco <caiofranco@skipp.com.br>
2019-11-05 22:45:38 -0300

    Tela Welcome - Finalizada. Termos de uso. Ações dos Botões. Ajustes de interfaces.

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -6,11 +6,11 @@
 import React, {Component} from 'react';
 
 /* Presentational */
-import { Animated, FlatList, StyleSheet, Image, Dimensions} from 'react-native';
+import {Modal, Animated, FlatList, StyleSheet, Image, Dimensions, ScrollView} from 'react-native';
 import {Block, Text, Button} from '../components';
 import {theme} from '../constants';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 class Welcome extends Component {
     static navigationOptions = {
@@ -19,13 +19,55 @@ class Welcome extends Component {
 
     scrollX = new Animated.Value(0);
     state = {
-
+        showTerms: false,
     };
 
-    renderIllustrations() {
-        const { illustrations } = this.props;
+    renderTermsService() {
         return (
-           <FlatList
+            <Modal animationType={'slide'} visible={this.state.showTerms}>
+                <Block padding={[theme.sizes.padding * 2, theme.sizes.padding]} space={'between'}>
+                    <Text h2 light>Terms of Service</Text>
+                    <ScrollView style={{paddingVertical: theme.sizes.padding}}>
+                        <Text caption gray height={18}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque dolorem, dolorum
+                            eaque ex explicabo facilis laborum minus, nemo nihil placeat quis quod repellendus sint
+                            tenetur veniam voluptatem voluptatibus.
+                        </Text>
+                        <Text caption gray height={18}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque dolorem, dolorum
+                            eaque ex explicabo facilis laborum minus, nemo nihil placeat quis quod repellendus sint
+                            tenetur veniam voluptatem voluptatibus.
+                        </Text>
+                        <Text caption gray height={18}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque dolorem, dolorum
+                            eaque ex explicabo facilis laborum minus, nemo nihil placeat quis quod repellendus sint
+                            tenetur veniam voluptatem voluptatibus.
+                        </Text>
+                        <Text caption gray height={18}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque dolorem, dolorum
+                        eaque ex explicabo facilis laborum minus, nemo nihil placeat quis quod repellendus sint tenetur
+                        veniam voluptatem voluptatibus.
+                        </Text>
+                        <Text caption gray height={18}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque dolorem, dolorum
+                            eaque ex explicabo facilis laborum minus, nemo nihil placeat quis quod repellendus sint
+                            tenetur veniam voluptatem voluptatibus.
+                        </Text>
+                    </ScrollView>
+                    <Button gradient onPress={() => {
+                        this.setState({showTerms: false});
+                    }}>
+                        <Text center white>I undestand</Text>
+                    </Button>
+                </Block>
+            </Modal>
+        );
+    }
+
+    renderIllustrations() {
+        const {illustrations} = this.props;
+        return (
+            <FlatList
                 horizontal
                 pagingEnabled
                 scrollEnabled
@@ -39,30 +81,30 @@ class Welcome extends Component {
                     <Image
                         source={item.source}
                         resizeMode='contain'
-                        style={{ width, height: height / 2, overflow: 'visible' }}
+                        style={{width, height: height / 2, overflow: 'visible'}}
                     />
-                    )}
+                )}
                 onScroll={
                     Animated.event([
                         {
-                            nativeEvent: { contentOffset: { x: this.scrollX }}
-                        }
+                            nativeEvent: {contentOffset: {x: this.scrollX}},
+                        },
                     ])
                 }
-           />
+            />
         );
     }
 
     renderSteps() {
-        const { illustrations } = this.props;
+        const {illustrations} = this.props;
         const stepPosition = Animated.divide(this.scrollX, width);
         return (
             <Block row center middle style={styles.stepsContainer}>
                 {illustrations.map((item, index) => {
                     const opacity = stepPosition.interpolate({
-                        inputRange: [index-1, index, index+1],
+                        inputRange: [index - 1, index, index + 1],
                         outputRange: [0.4, 1, 0.4],
-                        extrapolate: 'clamp'
+                        extrapolate: 'clamp',
                     });
                     return (
                         <Block
@@ -70,8 +112,8 @@ class Welcome extends Component {
                             flex={false}
                             key={`step-${index}`}
                             color='gray'
-                            style={[ styles.steps, {opacity}]}/>
-                    )
+                            style={[styles.steps, {opacity}]}/>
+                    );
                 })}
 
             </Block>
@@ -79,14 +121,14 @@ class Welcome extends Component {
     }
 
     render() {
+        const {navigation} = this.props;
+
         return (
             <Block>
-                <Block center middle flex={0.5}>
+                <Block center bottom flex={0.4}>
                     <Text h1 center bold>
                         Your Home.
-                        <Text h1 primary>
-                            Greener
-                        </Text>
+                        <Text h1 primary> Greener</Text>
                     </Text>
                     <Text h3 gray2 style={{marginTop: theme.sizes.padding / 2}}>
                         Enjoy the experience.
@@ -97,19 +139,17 @@ class Welcome extends Component {
                     {this.renderSteps()}
                 </Block>
                 <Block middle flex={0.5} margin={[0, theme.sizes.padding * 2]}>
-                    <Button gradient onPress={() => {
-                    }}>
+                    <Button gradient onPress={() => navigation.navigate('Login')}>
                         <Text center semibold white>Login</Text>
                     </Button>
-                    <Button shadow onPress={() => {
-                    }}>
+                    <Button shadow onPress={() => navigation.navigate('Signup')}>
                         <Text center semibold>SignUp</Text>
                     </Button>
-                    <Button onPress={() => {
-                    }}>
+                    <Button onPress={() => this.setState({showTerms: true})}>
                         <Text center caption semibold gray>Terms of Service</Text>
                     </Button>
                 </Block>
+                {this.renderTermsService()}
             </Block>
         );
     }
@@ -130,7 +170,7 @@ const styles = StyleSheet.create({
         position: 'absolute',
         bottom: theme.sizes.base * 2,
         right: 0,
-        left: 0
+        left: 0,
     },
     steps: {
         width: 5,
